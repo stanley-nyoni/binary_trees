e1f8eb16832c17a9a612918e8d50675b0d5e5596
stanley-nyoni <mqelisinyoni77@gmail.com>
2023-11-02 18:24:30 +0000

    depth of a binary tree - corrections

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -9,13 +9,5 @@
 
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-	size_t left = 0, right = 0;
-
-	if (tree == NULL)
-		return (0);
-
-	left = tree->left ? 1 + binary_tree_depth(tree->left) : 0;
-	right = tree->right ? 1 + binary_tree_depth(tree->right) : 0;
-
-	return ((left > right) ? left : right);
+	return ((tree && tree->parent) ? 1 + binary_tree_depth(tree->parent) : 0);
 }
