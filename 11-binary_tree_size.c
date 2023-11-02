9cfe576cec566cf55903b0b3cc5cc4c1751d676f
stanley-nyoni <mqelisinyoni77@gmail.com>
2023-11-02 18:34:49 +0000

    get the size of a binary tree

---
+++ 11-binary_tree_size.c
@@ -0,0 +1,22 @@
+#include "binary_trees.h"
+
+/**
+* binary_tree_size - gets the size of a binary tree
+* @tree: pointer to the root node of a binary tree to measure size
+*
+* Return: size (size_t) else 0 if tree is NULL
+*/
+
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	size_t size = 0;
+
+	if (tree == NULL)
+		return (0);
+
+	size += 1;
+	size += binary_tree_size(tree->left);
+	size += binary_tree_size(tree->right);
+
+	return (size);
+}
